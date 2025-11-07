1c01bb5b585fb31d3764eb66974886ae3dfe01e8
Gael Tuby <youremail@example.com>
2025-11-07 11:22:05 +0300

    updated next js

--- my-portfolio/next.config.js
+++ my-portfolio/next.config.js
@@ -1,12 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/My-Portfolio_",
-  assetPrefix: "/My-Portfolio_",
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: "/<your-repo-name>",
+  assetPrefix: "/<your-repo-name>/",
 };
 
 export default nextConfig;
